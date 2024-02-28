b53b6dc97beea8bbb06a53cefe4e306ab423080a
Surreality-dev <70345809+Surreality-dev@users.noreply.github.com>
2024-02-28 00:14:10 -0500

    Added the interior mansion
    interior mansion is hooked up with a precondition for the door to be a male

---
+++ CustomMaps/assets/z_condominium.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="z_condominium" tilewidth="16" tileheight="16" tilecount="304" columns="16">
+ <image source="26_Condominium.png" width="256" height="304"/>
+</tileset>
